e10716a527edc3f24d2cbf207e49cb2f10969f3e
ifeli <felicienirwin>
2023-09-27 11:42:26 -0500

    x

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-document.getElementById("titleBar").innerHTML = "Irwin Felicien";
+document.getElementById("titleBar").innerText = "Irwin Felicien";
 
 // left side
-document.getElementById("leftTitle").innerHTML = "Left Side";
-document.getElementById("leftParagraph").innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore officiis placeat mollitia nesciunt consectetur? Quisquam quae voluptates laudantium eveniet fugiat, ducimus asperiores fuga impedit veritatis voluptate enim rem similique vero consectetur laboriosam mollitia praesentium excepturi quas libero distinctio dolore provident.";
-document.getElementById("leftPara2").innerHTML = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, corporis sed, at, quis aliquam rem repellendus harum repudiandae eius porro recusandae voluptates unde. Reiciendis itaque cum in minima autem commodi.";
+document.getElementById("leftTitle").innerText = "Left Side";
+document.getElementById("leftParagraph").innerText =
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore officiis placeat mollitia nesciunt consectetur? Quisquam quae voluptates laudantium eveniet fugiat, ducimus asperiores fuga impedit veritatis voluptate enim rem similique vero consectetur laboriosam mollitia praesentium excepturi quas libero distinctio dolore provident.";
+document.getElementById("leftPara2").innerText =
+  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, corporis sed, at, quis aliquam rem repellendus harum repudiandae eius porro recusandae voluptates unde. Reiciendis itaque cum in minima autem commodi.";
 
-// middle 
-document.getElementById("middleTitle").innerHTML = "Main Content";
-document.getElementById("middleParagraph").innerHTML = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, assumenda placeat voluptate libero aliquid inventore nihil ab, vitae iure veritatis, obcaecati ducimus voluptates numquam impedit? Corporis totam, aliquam repellat dolores nemo animi impedit, earum id commodi repellendus dicta, eaque enim consectetur quis accusantium! Rem architecto natus dicta veritatis expedita error dolore hic mollitia, non, magni quibusdam sequi suscipit, ipsum sed?";
+// middle
+document.getElementById("middleTitle").innerText = "Main Content";
+document.getElementById("middleParagraph").innerText =
+  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, assumenda placeat voluptate libero aliquid inventore nihil ab, vitae iure veritatis, obcaecati ducimus voluptates numquam impedit? Corporis totam, aliquam repellat dolores nemo animi impedit, earum id commodi repellendus dicta, eaque enim consectetur quis accusantium! Rem architecto natus dicta veritatis expedita error dolore hic mollitia, non, magni quibusdam sequi suscipit, ipsum sed?";
 
 // right side
-document.getElementById("rightTitle").innerHTML = "Right Side";
-document.getElementById("rightParagraph").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam incidunt facere, possimus, unde illo quis iusto nihil minus, officiis aperiam quod natus veritatis? Dolorum atque assumenda adipisci quae laboriosam consectetur magnam quam exercitationem unde necessitatibus tempore delectus, iste sequi aliquid?";
+document.getElementById("rightTitle").innerText = "Right Side";
+document.getElementById("rightParagraph").innerText =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam incidunt facere, possimus, unde illo quis iusto nihil minus, officiis aperiam quod natus veritatis? Dolorum atque assumenda adipisci quae laboriosam consectetur magnam quam exercitationem unde necessitatibus tempore delectus, iste sequi aliquid?";
