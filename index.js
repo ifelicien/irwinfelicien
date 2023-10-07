19ac1e97933522c6d58e592ca3570d20dc0ff946
irwin felicien <ifelicien@gmail.com>
2023-10-07 16:27:51 -0500

    made changes to font on home page and js file

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-document.querySelector("titleBar").innerText = "Irwin Felicien";
-
-// left side
-document.querySelector("leftTitle").innerText = "Left Side";
-document.querySelector("leftParagraph").innerText =
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore officiis placeat mollitia nesciunt consectetur? Quisquam quae voluptates laudantium eveniet fugiat, ducimus asperiores fuga impedit veritatis voluptate enim rem similique vero consectetur laboriosam mollitia praesentium excepturi quas libero distinctio dolore provident.";
-document.querySelector("leftPara2").innerText =
-  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, corporis sed, at, quis aliquam rem repellendus harum repudiandae eius porro recusandae voluptates unde. Reiciendis itaque cum in minima autem commodi.";
-
-// middle
-document.querySelector("middleTitle").innerText = "Main Content";
-document.querySelector("middleParagraph").innerText =
-  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, assumenda placeat voluptate libero aliquid inventore nihil ab, vitae iure veritatis, obcaecati ducimus voluptates numquam impedit? Corporis totam, aliquam repellat dolores nemo animi impedit, earum id commodi repellendus dicta, eaque enim consectetur quis accusantium! Rem architecto natus dicta veritatis expedita error dolore hic mollitia, non, magni quibusdam sequi suscipit, ipsum sed?";
-
-// right side
-document.querySelector("rightTitle").innerText = "Right Side";
-document.querySelector("rightParagraph").innerText =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam incidunt facere, possimus, unde illo quis iusto nihil minus, officiis aperiam quod natus veritatis? Dolorum atque assumenda adipisci quae laboriosam consectetur magnam quam exercitationem unde necessitatibus tempore delectus, iste sequi aliquid?";
+function myFunction() {
+    document.getElementById("middleParagraph").innerHTML = 'On a more personal note, I\'m a tech geek through and through. I love getting lost in video games, especially the thrilling world of sim racing and the competitive realm of fighting games. But it\'s not just about virtual adventures for me; I\'m equally passionate about building computers, crafting these powerful systems that fuel my digital escapades. When I\'m not knee-deep in code or hardware, you can find me outdoors, hitting the gym, playing sports, or biking along scenic routes. I\'m all about that balance, finding excitement in both the tech universe and the active, real-world experiences. Join me on this journey where the thrill of technology meets the buzz of active living!'
+}
